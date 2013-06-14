810db512237734af5ea2fd5d7b83ad4441a566b6
Pascal Hartig <phartig@rdrei.net>
2013-06-15 00:18:47 +0200

    Client start

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,36 @@
-/*global io */
+/*global io, StateMachine */
 (function () {
-  'use strict'
+  'use strict';
 
   var socket = io.connect('http://localhost:8080');
+  var ConnectionState = function () {
+    this.startup();
+  };
 
-  function onConnected() {
-    console.log('Connected.');
+  StateMachine.create({
+    target: ConnectionState.prototype,
+    events: [
+      { name: 'startup', from: 'none', to: 'connecting' },
+      { name: 'connected', from: 'connecting', to: 'connected' }
+    ]
+  });
+
+  function Player() {
+    this.uuid = null;
+    this.state = new ConnectionState();
+  }
+
+  var players = {
+    self: new Player()
+  };
+
+  function onConnected(data) {
+    var self = players.self;
+
+    self.uuid = data.uuid;
+    self.state.connected();
+
+    console.log('Player state: ', self.state.current);
   }
 
   socket.on('connected', onConnected);
