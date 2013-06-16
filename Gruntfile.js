c9e4bd18ba7a74e447c55f6aee1c34883717bd34
Pascal Hartig <phartig@rdrei.net>
2013-06-16 13:24:40 +0200

    Fixed css build step for separate main/frame

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,8 +224,10 @@ module.exports = function (grunt) {
       dist: {
         files: {
           '<%= yeoman.dist %>/styles/main.css': [
-            '.tmp/styles/{,*/}*.css',
-            '<%= yeoman.app %>/styles/{,*/}*.css'
+            '.tmp/styles/main.css'
+          ],
+          '<%= yeoman.dist %>/styles/frame.css': [
+            '.tmp/styles/frame.css'
           ]
         }
       }
