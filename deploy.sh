16d457e90d571c76f248d39e6a947fd46ca289e5
Tobias Reese <tobias.reese@phimobile.com>
2013-06-16 12:57:14 +0200

    - added .files to rsync

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 echo "This file need environment Variables"
 
 echo "Copy Files to $TARGET_HOST:$TARGET_DIR"
-rsync --delete-delay -vrz --progress * $TARGET_HOST:$TARGET_DIR || exit 1
+rsync --delete-delay -vrz --progress * .bowerrc .jshintrc $TARGET_HOST:$TARGET_DIR || exit 1
 ssh $TARGET_HOST "cd $TARGET_DIR && bash install.sh"
